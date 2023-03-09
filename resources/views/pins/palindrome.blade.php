5ffb15855954b3aa2c364e1c5afb09141bea03d9
Joe McNelis <josephmcnelis@googlemail.com>
2023-03-09 23:16:41 +0000

    Adding some basic styling to endpoints

--- resources/views/pins/palindrome.blade.php
+++ resources/views/pins/palindrome.blade.php
@@ -1,6 +1,30 @@
-<h1>Is it a palindrome?</h1>
-@if ($outcome === true)
-    NOT ITS NOT
-@else
-    YES IT IS
-@endif
+<head>
+    <title>Pin Generator - Generate</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container mt-4">
+    @if(session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+    @endif
+    <div class="card">
+        <div class="card-header text-center font-weight-bold">
+            Is it a palindrome?
+        </div>
+
+        <div class="card-body">
+
+            @if ($outcome === true)
+                NO it's not.
+            @else
+                YES it is!
+            @endif
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
--- resources/views/pins/repeating.blade.php
+++ resources/views/pins/repeating.blade.php
@@ -1,6 +1,28 @@
-<h1>Is it a repeating number?</h1>
-@if ($outcome === true)
-    NO ITS NOT
-@else
-    YES IT IS
-@endif
+<head>
+    <title>Pin Generator - Generate</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container mt-4">
+    @if(session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+    @endif
+    <div class="card">
+        <div class="card-header text-center font-weight-bold">
+            Is it a repeating number?
+        </div>
+
+        <div class="card-body">
+            @if ($outcome === true)
+                NO it's not.
+            @else
+                YES it is!
+            @endif
+        </div>
+    </div>
+</div>
+</body>
+</html>
